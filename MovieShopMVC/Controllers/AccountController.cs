b843858ddcb55b8060197f12c5ca1b66559d3dfa
666wen <105310343+666wen@users.noreply.github.com>
2022-06-14 09:27:09 -0700

    update app

---
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieShopMVC/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
